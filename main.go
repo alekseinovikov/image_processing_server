d419ca92dcc3c23fd580e0008a2cb05ba6a4217d
AlexVladimirovZensoft <alexey.novikov@zensoft.io>
2017-09-13 10:56:16 +0600

    Added commandline argument parser and simple http server with resolver func

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"flag"
+	"net/http"
+	_ "github.com/freedom4live/resize"
+)
 
+
+// Stores configs of the server
+type ServerConfigs struct {
+	port string
+}
+
+
+// Parses command line arguments and puts them into the ServerConfigs structure
+func parseArgsToConfigs() ServerConfigs {
+	portPtr := flag.String("port", "8080", "The portPtr of the server")
+	flag.Parse()
+
+	return ServerConfigs{port: *portPtr}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+
+//Entry point of the program
 func main() {
-	fmt.Println("Hello!")
+	configs:= parseArgsToConfigs()
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":" + configs.port, nil)
 }
