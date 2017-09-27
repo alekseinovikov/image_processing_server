0ef0c905aad36a0ffee41f5b831f0eb343e0262b
AlexVladimirovZensoft <alexey.novikov@zensoft.io>
2017-09-27 11:47:36 +0600

    Now it works for resizing and thumbnails need receive width and height from the request

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"flag"
 	"net/http"
-	_ "github.com/freedom4live/resize"
 	"strings"
+	"errors"
+	"github.com/freedom4live/image_processing_server/resizing"
+	"mime/multipart"
+	"strconv"
 )
 
 // Stores configs of the server
@@ -14,36 +16,107 @@ type ServerConfigs struct {
 }
 
 const MESSAGE_METHOD_NOT_ALLOWED = "Method is not allowed! Supports POST only!"
+const _24K = (1 << 10) * 24
 
 // Parses command line arguments and puts them into the ServerConfigs structure
 func parseArgsToConfigs() ServerConfigs {
-	portPtr := flag.String("port", "8080", "The portPtr of the server")
+	portPtr := flag.String("port", "8082", "The portPtr of the server")
 	flag.Parse()
 
 	return ServerConfigs{port: *portPtr}
 }
 
-// Handler for resizing image
-func resizeHandler(w http.ResponseWriter, r *http.Request) {
-	if strings.ToUpper(r.Method) != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte(MESSAGE_METHOD_NOT_ALLOWED))
-
+// Opens multipart request and returns first file as multipart
+func getMultipartFile(req http.Request) (file multipart.File, status int, response string, err error) {
+	if err = req.ParseMultipartForm(_24K); nil != err {
+		status = http.StatusInternalServerError
 		return
 	}
 
+	for _, fheaders := range req.MultipartForm.File {
+		for _, hdr := range fheaders {
+
+			// open uploaded
+			if file, err = hdr.Open(); nil != err {
+				status = http.StatusInternalServerError
+				response = "Can't open uploading file!"
+				return
+			}
+
+			return
+		}
+	}
+
+	status = http.StatusUnprocessableEntity
+	response = "Can't find name of file!"
+	return
+}
+
+// Checks method and write answer to the client
+func checkMethodPOSTAndWriteAnswer(w http.ResponseWriter, r *http.Request) error {
+	if strings.ToUpper(r.Method) != "POST" {
+		writeError(http.StatusMethodNotAllowed, MESSAGE_METHOD_NOT_ALLOWED, w)
+		return errors.New(MESSAGE_METHOD_NOT_ALLOWED)
+	}
+
+	return nil
+}
+
+// Writes status and response to the client
+func writeError(status int, response string, w http.ResponseWriter) {
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+}
+
+// Writes bytes to the response
+func writeFile(bytes []byte, w http.ResponseWriter, r http.Request) {
+	w.Header().Set("Content-Disposition", "attachment; filename=result.jpg")
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", strconv.Itoa(len(bytes)))
+
+	w.Write(bytes)
+}
+
+// Handler for resizing image
+func resizeHandler(w http.ResponseWriter, r *http.Request) {
+	if nil != checkMethodPOSTAndWriteAnswer(w, r) {
+		return
+	}
+
+	file, status, response, err := getMultipartFile(*r)
+	if nil != err {
+		writeError(status, response, w)
+		return
+	}
+
+	bytes, err := resizing.Resize(file)
+	if nil != err {
+		writeError(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	writeFile(bytes, w, *r)
 }
 
 // Handler for creating thumbnail
 func thumbnailHandler(w http.ResponseWriter, r *http.Request) {
-	if strings.ToUpper(r.Method) != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte(MESSAGE_METHOD_NOT_ALLOWED))
-
+	if nil != checkMethodPOSTAndWriteAnswer(w, r) {
 		return
 	}
 
-	fmt.Fprint(w, "Hello thumbnail")
+	file, status, response, err := getMultipartFile(*r)
+	if nil != err {
+		writeError(status, response, w)
+		return
+	}
+
+	bytes, err := resizing.Thumbnail(file)
+	if nil != err {
+		writeError(http.StatusInternalServerError, err.Error(), w)
+		return
+	}
+
+	writeFile(bytes, w, *r)
 }
 
 //Entry point of the program
--- resizing/resizing.go
+++ resizing/resizing.go
@@ -1,9 +1,44 @@
 package resizing
 
-func Resize()  {
+import (
+	"mime/multipart"
+	"image"
+	"image/jpeg"
+	"github.com/freedom4live/resize"
+	"bytes"
+)
 
+// Gets image from the multipart file
+func getImage(file multipart.File) (image.Image, error) {
+	defer file.Close()
+
+	return jpeg.Decode(file)
 }
 
-func Thumbnail() {
+// Resizing file
+func Resize(file multipart.File) ([]byte, error) {
+	img, err := getImage(file)
+	if nil != err {
+		return nil, err
+	}
 
+	newImage := resize.Resize(100, 100, img, resize.Lanczos3)
+
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, newImage, nil)
+	return buf.Bytes(), nil
+}
+
+// Making thumbnail
+func Thumbnail(file multipart.File) ([]byte, error) {
+	img, err := getImage(file)
+	if nil != err {
+		return nil, err
+	}
+
+	newImage := resize.Thumbnail(100, 100, img, resize.Lanczos3)
+
+	buf := new(bytes.Buffer)
+	err = jpeg.Encode(buf, newImage, nil)
+	return buf.Bytes(), nil
 }
